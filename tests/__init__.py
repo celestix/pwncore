f29883b80b3a2dc7e16030d28badbf2e2663b911
celestix <i@anodite.me>
2024-09-13 20:16:10 +0530

    refactor: move pytest fixture to a common module

---
+++ tests/__init__.py
@@ -0,0 +1,29 @@
+import pytest
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from asgi_lifespan import LifespanManager
+from httpx import ASGITransport, AsyncClient
+
+from pwncore import app
+
+ClientManagerType = AsyncGenerator[AsyncClient, None]
+
+@pytest.fixture(scope="module")
+def anyio_backend() -> str:
+    return "asyncio"
+
+
+@asynccontextmanager
+async def client_manager(app, base_url="http://test", **kw) -> ClientManagerType:
+    app.state.testing = True
+    async with LifespanManager(app):
+        transport = ASGITransport(app=app)
+        async with AsyncClient(transport=transport, base_url=base_url, **kw) as c:
+            yield c
+
+
+@pytest.fixture(scope="module")
+async def client() -> ClientManagerType:
+    async with client_manager(app) as c:
+        yield c
---
+++ tests/test_leaderboard.py
@@ -0,0 +1,8 @@
+from tests import *
+
+@pytest.mark.anyio
+async def test_login(client: AsyncClient):
+    # Send a GET response to the specified endpoint
+    response = await client.get("/api/leaderboard")
+    # Evaluate the response against expected values
+    assert response.text == "[]"
